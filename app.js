8cb61f8b2643bc6b0828d16dbdc61c5a627ad4a0
Robert <robert913152750@gmail.com>
2020-06-30 14:31:58 +0800

    Initalize project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("this will be a restaurant-forum");
+});
