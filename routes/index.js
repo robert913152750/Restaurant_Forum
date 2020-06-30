33b4658ed248de287280bfd3572c515828e13962
Robert <robert913152750@gmail.com>
2020-06-30 17:42:34 +0800

    Install bcryptjs, body-parser and add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require("../controllers/restController.js");
 const adminController = require("../controllers/adminController");
+const userController = require("../controllers/userController");
 
 module.exports = (app) => {
   //前台路由
@@ -8,6 +9,10 @@ module.exports = (app) => {
   app.get("/restaurants", restController.getRestaurants);
   //後台路由
   app.get("/admin", (req, res) => res.redirect("/admin/restaurants"));
+
+  //註冊路由
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
 
   app.get("/admin/restaurants", adminController.getRestaurants);
 };
