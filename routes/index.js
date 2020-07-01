7e7fe5c5bb3aa6b1639f070abaecdc35764c6e3c
Robert <robert913152750@gmail.com>
2020-07-01 16:04:08 +0800

    Add isAdmin to users table and create authentication feature

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,40 @@ const adminController = require("../controllers/adminController");
 const userController = require("../controllers/userController");
 
 module.exports = (app, passport) => {
-  //前台路由
-  app.get("/", (req, res) => res.redirect("/restaurants"));
+  //身分驗證middleware
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/signin");
+  };
+  const authenticateAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next();
+      }
+      return res.redirect("/");
+    }
+    res.redirect("/signin");
+  };
 
-  app.get("/restaurants", restController.getRestaurants);
+  //前台路由
+  app.get("/", authenticated, (req, res) => res.redirect("/restaurants"));
+  app.get("/restaurants", authenticated, restController.getRestaurants);
+
   //後台路由
-  app.get("/admin", (req, res) => res.redirect("/admin/restaurants"));
+  app.get("/admin", authenticateAdmin, (req, res) =>
+    res.redirect("/admin/restaurants")
+  );
+  app.get(
+    "/admin/restaurants",
+    authenticateAdmin,
+    adminController.getRestaurants
+  );
 
   //註冊路由
   app.get("/signup", userController.signUpPage);
   app.post("/signup", userController.signUp);
-
   app.get("/admin/restaurants", adminController.getRestaurants);
 
   //登入驗證路由
