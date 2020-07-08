3f6117ee47dc7e365ff2de73b111153ad678fcd5
Robert <robert913152750@gmail.com>
2020-07-08 17:38:08 +0800

    Show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require("../models");
 const Restaurant = db.Restaurant;
 const Category = db.Category;
+const Comment = db.Comment;
+const User = db.User;
 const pageLimit = 10;
 
 let restController = {
@@ -54,13 +56,14 @@ let restController = {
   },
   //瀏覽餐廳個別資料
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { include: Category }).then(
-      (restaurant) => {
-        return res.render("restaurant", {
-          restaurant: restaurant.toJSON(),
-        });
-      }
-    );
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category, { model: Comment, include: [User] }],
+    }).then((restaurant) => {
+      console.log(restaurant.Comments[0].dataValues);
+      return res.render("restaurant", {
+        restaurant: restaurant.toJSON(),
+      });
+    });
   },
 };
 module.exports = restController;
