69cc33f764a98761120dbb385da2f08051e8d4e2
Robert <robert913152750@gmail.com>
2020-07-08 16:43:15 +0800

    Create comment model and add association among users, comment, restaurants

--- models/comment.js
+++ models/comment.js
@@ -1,27 +1,27 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Comment);
+      Comment.belongsTo(models.Restaurant);
+      Comment.belongsTo(models.User);
     }
   }
-  User.init(
+  Comment.init(
     {
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      isAdmin: DataTypes.BOOLEAN,
+      text: DataTypes.STRING,
+      UserId: DataTypes.INTEGER,
+      RestaurantId: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "Comment",
     }
   );
-  return User;
+  return Comment;
 };
--- models/restaurant.js
+++ models/restaurant.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Restaurant.belongsTo(models.Category);
+      Restaurant.hasMany(models.Comment);
     }
   }
   Restaurant.init(
