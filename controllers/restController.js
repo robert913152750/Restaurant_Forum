0550bfd09d4e1be7ed2642029861448cc3f27e26
Robert <robert913152750@gmail.com>
2020-07-08 16:17:02 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,35 +1,56 @@
 const db = require("../models");
 const Restaurant = db.Restaurant;
 const Category = db.Category;
+const pageLimit = 10;
 
 let restController = {
   //瀏覽餐廳總表
   getRestaurants: (req, res) => {
+    let offset = 0;
     let whereQuery = {};
     let categoryId = "";
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit;
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId);
       whereQuery["CategoryId"] = categoryId;
     }
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(
-      (restaurants) => {
-        const data = restaurants.map((r) => ({
-          ...r.dataValues,
-          description: r.dataValues.description.substring(0, 50),
-          categoryName: r.Category.name,
-        }));
-        Category.findAll({
-          raw: true,
-          nest: true,
-        }).then((categories) => {
-          return res.render("restaurants", {
-            restaurants: data,
-            categories: categories,
-            categoryId: categoryId,
-          });
+    Restaurant.findAndCountAll({
+      include: Category,
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit,
+    }).then((result) => {
+      let page = Number(req.query.page) || 1;
+      let pages = Math.ceil(result.count / pageLimit);
+      let totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1
+      );
+
+      let prev = page - 1 < 1 ? 1 : page - 1;
+      let next = page + 1 > pages ? pages : page + 1;
+
+      const data = result.rows.map((r) => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.Category.name,
+      }));
+      Category.findAll({
+        raw: true,
+        nest: true,
+      }).then((categories) => {
+        return res.render("restaurants", {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next,
         });
-      }
-    );
+      });
+    });
   },
   //瀏覽餐廳個別資料
   getRestaurant: (req, res) => {
